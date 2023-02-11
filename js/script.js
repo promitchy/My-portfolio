046ddbb34c6ab118789f98b8f86f5a8ab2d6ae9e
promitchy <promitchy@engineer.com>
2023-02-11 14:11:07 +0600

    animation-typing

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typind Animation
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
